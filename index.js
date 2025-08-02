a8b69979b9d85a011571d02a451c5f3189a27e2f
MatiasAlbites2004 <matiasalbites1512@gmail.com>
2025-08-01 21:27:10 -0500

    feat: API Linktree

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 import routes from "./src/routes/index.routes.js";
+import linktreeRoutes from "./src/routes/linktree.routes.js";
+
 
 const app = express();
 const PORT = 3001;
@@ -12,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", routes);
+app.use("/api/linktree", linktreeRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
