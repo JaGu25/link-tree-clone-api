43bea801b9a414c32c6c8bd90152865e47b167e8
MatiasAlbites2004 <matiasalbites1512@gmail.com>
2025-08-13 13:40:00 -0500

    feat: Upload Files Multer Be

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 import routes from "./src/routes/index.routes.js";
+import uploadRoutes from "./src/routes/uplodad.routes.js";
 
 const app = express();  
 const PORT = 3001;
 
 app.use(cors());
 app.use(express.json());  
+
+app.use(express.static("public"));
+app.use("/api/upload-avatar", uploadRoutes);
+
 
 app.use("/api", routes);  
 
